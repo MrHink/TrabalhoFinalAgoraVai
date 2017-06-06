f37b4181231ac9ed250d2c0fb39af057b394c849
MrHink <14104919@W1815026.portoalegre.pucrsnet.br>
2017-06-06 20:42:19 -0300

    Mada

---
+++ src/Dados/User.java
@@ -0,0 +1,41 @@
+package Dados;
+
+public class User {
+	
+	private String nome;
+	private String codigoIdentificador;
+	private String email;
+	
+	public User(String n, String ci, String e){
+		this.nome = n;
+		this.codigoIdentificador = ci;
+		this.email = e;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCodigoIdentificador() {
+		return codigoIdentificador;
+	}
+
+	public void setCodigoIdentificador(String codigoIdentificador) {
+		this.codigoIdentificador = codigoIdentificador;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
