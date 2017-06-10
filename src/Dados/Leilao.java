99e0094bc00b302a650b56e942d0964a3a721ceb
MrHink <hink9999@hotmail.com>
2017-06-10 16:10:00 -0300

    Leilão modificação

--- src/Dados/Leilao.java
+++ src/Dados/Leilao.java
@@ -2,4 +2,10 @@ package Dados;
 
 public class Leilao {
 
+	private boolean leilao;
+	private boolean tipoLeilao;
+	public Leilao(boolean l, boolean tl){
+		this.leilao = l;
+		this.tipoLeilao = tl;		
+	}
 }
